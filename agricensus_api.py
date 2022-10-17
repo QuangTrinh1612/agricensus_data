c756f669f436dd07d351bb5ddb76adeac18e7959
QuangTrinh1612 <trinhquocquang.buh@gmail.com>
2022-10-17 16:05:22 +0700

    20221017 - Updated data source and csv format

--- agricensus_api.py
+++ agricensus_api.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 from datetime import date as dt
 
-def get_url(token: str, feed_type: str, file_format: str = 'JSON') -> str:
+def get_url(token: str, feed_type: str, file_format: str = 'CSV') -> str:
     feed_type_dict = {  
         'daily_export': '',
         'historical': 'historical',
@@ -12,28 +12,22 @@ def get_url(token: str, feed_type: str, file_format: str = 'JSON') -> str:
     url = f'https://www.agricensus.com/feed/?token={token}&format={file_format}&{feed_type_dict[feed_type]}'
     return url
 
-def get_agricensus_api(token: str, feed_type: str = 'daily_export', file_format: str = 'JSON'):
+def get_agricensus_data(token: str, feed_type: str = 'daily_export', file_format: str = 'CSV') -> pd.DataFrame:
+
     url = get_url(token, feed_type, file_format)
-    response = requests.get(
-        url = url,
-        headers = {
-            'Accept': 'application/csv',
-            'Content-Type': 'text/csv'
-        }
-    )
-    return response
 
-def get_agricensus_data(token: str, feed_type: str = 'daily_export', file_format: str = 'JSON') -> pd.DataFrame:
-    file_name_dict = {  
-        'daily_export': 'daily_price',
-        'historical': 'historical_price',
-        'full_historical': 'full_historical_price',
-        'specs_export': 'agricensus_specs'
-    }
+    if file_format == 'JSON':
+        # GET API Request
+        response = requests.get(
+            url = url,
+            headers = {
+                'Accept': f'application/json',
+                'Content-Type': f'application/json'
+            }
+        )
+        df = pd.json_normalize(response.json())
 
-    # GET API Request
-    response = get_agricensus_api(token, feed_type, file_format)
-
-    df = pd.json_normalize(response.json())
-
+    elif file_format == 'CSV':
+        df = pd.read_csv(url)
+    
     return df
--- main.py
+++ main.py
@@ -6,9 +6,28 @@ import re
 from datetime import date as dt
 from pathlib import Path  
 
+def write_to_csv(df: pd.DataFrame, feed_type: str):
+    
+    # Based on feed_type to get file_name
+    file_name_dict = {  
+        'daily_export': 'DailyExport',
+        'historical_export': 'HistoricalExport',
+        'specs_export': 'SpecsExport'
+    }
+    
+    file_path = Path(f'Data/{file_name_dict[feed_type]}_{dt.today().year*10000+dt.today().month*100+dt.today().day}.csv')  
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    df.to_csv(file_path, index=False)
+
 def data_transformation(is_export_to_csv: bool = True, is_upload_to_gdrive: bool = True) -> pd.DataFrame:
-    tx_df = agricensus_api.get_agricensus_data(config.token, 'daily_export', 'JSON')
-    specs_df = agricensus_api.get_agricensus_data(config.token, 'specs_export', 'JSON')
+    
+    # Daily Export Price
+    # tx_df = agricensus_api.get_agricensus_data(config.token, 'daily_export', 'CSV')
+    
+    # Historical Export Price
+    tx_df = agricensus_api.get_agricensus_data(config.token, 'full_historical', 'CSV')
+
+    specs_df = agricensus_api.get_agricensus_data(config.token, 'specs_export', 'CSV')
 
     # SPM and ARG/BRZ Only
     specs_df = specs_df[
@@ -22,37 +41,52 @@ def data_transformation(is_export_to_csv: bool = True, is_upload_to_gdrive: bool
     final_df = pd.DataFrame()
 
     # MAIN TRANSFORMATION
-    final_df['offer_date'] = pd.to_datetime(full_df.date)
-    final_df['commodity'] = np.where(full_df['Market'] == 'Corn', 'CORN',
+    final_df['DATE OFFER'] = pd.to_datetime(full_df.date)
+    final_df['COMMODITY'] = np.where(full_df['Market'] == 'Corn', 'CORN',
                             np.where(full_df['Market'].isin(['Soybean', 'Soymeal']), 'SBM', 'Others'))
-    final_df['comm'] = full_df['name'].apply(lambda name: re.sub('M[1-9] ', '', name))
-    final_df['currency'] = full_df['Currency']
-    final_df['m_month'] = np.where(full_df['Structure'] == 'SP', 'M0', full_df['Structure'])
-    final_df['origin'] = np.where(full_df['Name'].str.contains('Argentina'), 'ARG', 'BRZ')
-    final_df['seller'] = 'Agricensus API' # Need to check
-    final_df['region'] = 'SA' # Need to check - South America
-    final_df['price_type'] = 'CNF'
-    final_df['note'] = ''
-    final_df['price'] = full_df['value']
-    final_df['unit_price'] = np.where(full_df['name'].str.contains('/mt|/st'), 'flat',
-                            np.where(full_df['name'].str.contains('/bu'), 'basic', 'Unknown'))
+    
+    # 20221017 - Change Source Data
+    # final_df['comm'] = full_df['Name'].apply(lambda name: re.sub('M[1-9] ', '', name))
+    final_df['COMM'] = full_df['Market'] + ' ' + full_df['Name'].apply(lambda name: re.sub(' M[1-9]', '', name)) + ' ' + full_df['Currency'] + '/' + \
+                        np.where(full_df['Units'] == 'bushel (corn)', 'buc',
+                        np.where(full_df['Units'] == 'bushel (wheat/soy)', 'buw',
+                        np.where(full_df['Units'] == 'Metric tonnes', 'mt',
+                        np.where(full_df['Units'] == 'Short tons', 'st', full_df['Units']))))
+    
+    final_df['CURRENCY'] = full_df['Currency']
+    final_df['M_MONTH'] = full_df['shipment_delivery_month'] + ' ' + np.where(full_df['Structure'] == 'SP', 'M0', full_df['Structure'])
+    final_df['ORIGIN'] = np.where(full_df['Name'].str.contains('Argentina'), 'ARG', 'BRZ')
+    final_df['SELLER'] = np.where(full_df['Name'].str.contains('Argentina'), 'ARG', 'BRZ') # Need to check how to identify seller
+    final_df['REGION'] = 'SA' # Need to check - South America
+    final_df['PRICE_TYPE'] = 'CNF'
+    final_df['NOTE'] = ''
+    final_df['PRICE'] = full_df['value']
+    
+    # 20221017 - Change Source Data
+    # final_df['unit_price'] = np.where(full_df['comm'].str.contains('/mt|/st'), 'flat',
+    #                         np.where(full_df['comm'].str.contains('/bu'), 'basic', 'Unknown'))
 
-    final_df['shipment_date'] = final_df.apply(lambda row: row['offer_date'] + pd.DateOffset(months=1) if row['m_month'] == 'M0' or row['m_month'] == 'M1' else (
-                                                        row['offer_date'] + pd.DateOffset(months=2) if row['m_month'] == 'M2' else (
-                                                        row['offer_date'] + pd.DateOffset(months=3) if row['m_month'] == 'M3' else (
-                                                        row['offer_date'] + pd.DateOffset(months=4) if row['m_month'] == 'M4' else (
-                                                        row['offer_date'] + pd.DateOffset(months=5) if row['m_month'] == 'M5' else (
-                                                        row['offer_date'] + pd.DateOffset(months=6) if row['m_month'] == 'M6' else (
-                                                        row['offer_date'] + pd.DateOffset(months=7) if row['m_month'] == 'M7' else (
-                                                        row['offer_date'] + pd.DateOffset(months=8) if row['m_month'] == 'M8' else row['offer_date'] + pd.DateOffset(months=9)))))))),
+    final_df['UNIT PRICE'] = np.where(full_df['Units'] == 'bushel (corn)', 'basic',
+                            np.where(full_df['Units'] == 'bushel (wheat/soy)', 'basic',
+                            np.where(full_df['Units'] == 'Metric tonnes', 'flat',
+                            np.where(full_df['Units'] == 'Short tons', 'flat', 'Unknown'))))
+
+    final_df['SHIPMENT'] = final_df.apply(lambda row: row['DATE OFFER'] + pd.DateOffset(months=1) if row['M_MONTH'] == 'M0' or row['M_MONTH'] == 'M1' else (
+                                                        row['DATE OFFER'] + pd.DateOffset(months=2) if row['M_MONTH'] == 'M2' else (
+                                                        row['DATE OFFER'] + pd.DateOffset(months=3) if row['M_MONTH'] == 'M3' else (
+                                                        row['DATE OFFER'] + pd.DateOffset(months=4) if row['M_MONTH'] == 'M4' else (
+                                                        row['DATE OFFER'] + pd.DateOffset(months=5) if row['M_MONTH'] == 'M5' else (
+                                                        row['DATE OFFER'] + pd.DateOffset(months=6) if row['M_MONTH'] == 'M6' else (
+                                                        row['DATE OFFER'] + pd.DateOffset(months=7) if row['M_MONTH'] == 'M7' else (
+                                                        row['DATE OFFER'] + pd.DateOffset(months=8) if row['M_MONTH'] == 'M8' else row['DATE OFFER'] + pd.DateOffset(months=9)))))))),
                                             axis=1)
 
-    final_df['delivery_date'] = final_df['shipment_date'] + pd.DateOffset(months=2)
+    final_df['DELIVERY'] = final_df['SHIPMENT'] + pd.DateOffset(months=2)
 
     if is_export_to_csv == True:
-        file_path = Path(f'Data/DailyExport{dt.today().year*10000+dt.today().month*100+dt.today().day}.csv')  
-        file_path.parent.mkdir(parents=True, exist_ok=True)
-        final_df.to_csv(file_path, index=False)
+        write_to_csv(final_df, 'daily_export')
+        write_to_csv(final_df, 'historical_export')
+        # write_to_csv(specs_df, 'specs_export')
 
     return final_df
 
